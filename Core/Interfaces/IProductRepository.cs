4e2f2c3b4ab9b9660c8a1d5ea955e07f53d82321
Marina Zelenskaya <60691459+zelensskaya@users.noreply.github.com>
2023-03-14 21:28:00 +0100

    Task 23

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+using Core.Entities;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+    }
+}
