ffb24381fc83509c5e0f54593a7168a725841c84
Marina Zelenskaya <60691459+zelensskaya@users.noreply.github.com>
2023-03-25 21:29:19 +0100

    End of section 5

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse (int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode {get; set;}
+        public string Message {get; set;}
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request, you have made",
+                401 => "Authorised, you are not",
+                404 => "Resourse found, it was not",
+                500 => "Errors are the path to the dark side",
+                _ => null
+            };
+        }
+    }
+    
+    
+}
